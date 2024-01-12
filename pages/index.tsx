c5ae528d2f54e41aa628ac9785abe4a73d6271fd
akasen <akasen.org@gmail.com>
2024-01-13 04:59:20 +0900

    some changes

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,38 @@
-import {NextPage} from "next"
+import {GetStaticProps, NextPage} from "next"
+import fetch from "cross-fetch"
+import {BooksList} from "../components/models/book/booksList"
 import {Layout} from "../components/layout/layout"
+import Head from "next/head"
 import {Box} from "@mui/material"
-import Link from "next/link"
 
-const Page: NextPage = () => {
+export const getStaticProps: GetStaticProps = async () => {
+  const data = await fetch("https://atril2lirta.blob.core.windows.net/books/titles.json").then(res => res.json())
+  return {
+    props: {
+      books: data
+    },
+    revalidate: 100,
+  }
+}
+
+type Props = {
+  books: {[id: string]: {
+      title: string,
+      metadata: {
+        author: string,
+        description: string,
+        createdAt: string,
+      }
+    }}
+}
+
+const Page: NextPage<Props> = (props: Props) => {
   return <Layout>
-    <Box m={3}>
-      <Link href={"/books"}>books</Link>
-    </Box>
-    <Box m={3}>
-      <Link href={"/dictionary"}>dictionary</Link>
+    <Head>
+      <title>Atril - Choose a book</title>
+    </Head>
+    <Box sx={{maxWidth: 1000, marginLeft: "auto", marginRight: "auto"}}>
+      <BooksList books={props.books} />
     </Box>
   </Layout>
 }
